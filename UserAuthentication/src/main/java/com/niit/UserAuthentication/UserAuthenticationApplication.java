841e7ea3efa657ab22b8076da3b89a869b4f12f5
Nikita Chauhan <nikitachauhan64@gmail.com>
2023-01-02 18:23:33 +0530

    created the spring boot project for Eureka Server and added the docker-compose.yml file

--- UserAuthentication/src/main/java/com/niit/UserAuthentication/UserAuthenticationApplication.java
+++ UserAuthentication/src/main/java/com/niit/UserAuthentication/UserAuthenticationApplication.java
@@ -2,10 +2,12 @@ package com.niit.UserAuthentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UserAuthenticationApplication {
 
 	public static void main(String[] args) {
