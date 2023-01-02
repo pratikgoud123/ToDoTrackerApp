d04fdb8717d2033f3be1cf4aa002ba4ee1c01e3e
Pratik Goud <pratik.loop@gmail.com>
2023-01-02 15:30:10 +0530

    added UserTask Service in ToDoTrackerApp

---
+++ UserTask/src/test/java/com/niit/UserTask/UserTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.UserTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
