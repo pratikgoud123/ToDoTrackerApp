fccf013b462818ed60b996935b35edc611688b24
Pratik Goud <pratik.loop@gmail.com>
2023-01-13 21:11:19 +0530

    modified model classes

--- UserTask/src/main/java/com/niit/UserTask/domain/Task.java
+++ UserTask/src/main/java/com/niit/UserTask/domain/Task.java
@@ -1,100 +1,25 @@
 package com.niit.UserTask.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import javax.annotation.processing.Generated;
 import java.time.LocalDate;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     @Id
-    private  int taskId;
     private String taskName;
     private String taskContent;
+    private String imageURL;
     private LocalDate taskDeadline;
     private String taskCategory;
     private String taskPriorityLevel;
     private boolean isTaskCompleted;
 
-    public Task() {
-    }
-
-    public Task(int taskId, String taskName, String taskContent, LocalDate taskDeadline, String taskCategory, String taskPriorityLevel, boolean isTaskCompleted) {
-        this.taskId = taskId;
-        this.taskName = taskName;
-        this.taskContent = taskContent;
-        this.taskDeadline = taskDeadline;
-        this.taskCategory = taskCategory;
-        this.taskPriorityLevel = taskPriorityLevel;
-        this.isTaskCompleted = isTaskCompleted;
-    }
-
-    public int getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(int taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public String getTaskContent() {
-        return taskContent;
-    }
-
-    public void setTaskContent(String taskContent) {
-        this.taskContent = taskContent;
-    }
-
-    public LocalDate getTaskDeadline() {
-        return taskDeadline;
-    }
-
-    public void setTaskDeadline(LocalDate taskDeadline) {
-        this.taskDeadline = taskDeadline;
-    }
-
-    public String getTaskCategory() {
-        return taskCategory;
-    }
-
-    public void setTaskCategory(String taskCategory) {
-        this.taskCategory = taskCategory;
-    }
-
-    public String getTaskPriorityLevel() {
-        return taskPriorityLevel;
-    }
-
-    public void setTaskPriorityLevel(String taskPriorityLevel) {
-        this.taskPriorityLevel = taskPriorityLevel;
-    }
-
-    public boolean isTaskCompleted() {
-        return isTaskCompleted;
-    }
-
-    public void setTaskCompleted(boolean taskCompleted) {
-        isTaskCompleted = taskCompleted;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "taskId=" + taskId +
-                ", taskName='" + taskName + '\'' +
-                ", taskContent='" + taskContent + '\'' +
-                ", taskDeadline=" + taskDeadline +
-                ", taskCategory='" + taskCategory + '\'' +
-                ", taskPriorityLevel='" + taskPriorityLevel + '\'' +
-                ", isTaskCompleted=" + isTaskCompleted +
-                '}';
-    }
 }
--- UserTask/src/main/java/com/niit/UserTask/domain/User.java
+++ UserTask/src/main/java/com/niit/UserTask/domain/User.java
@@ -1,5 +1,8 @@
 package com.niit.UserTask.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,121 +10,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Arrays;
 import java.util.List;
 
+@Data
 @Document
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
-    @Transient
-    public static final String SEQUENCE_NAME = "user_sequence";
-    @Id
-    private int userId;
     private String firstName;
     private String lastName;
     private String file;
     private byte[] img;
+    private String imageURL;
+    @Id
     private String emailId;
     private String password;
     private String role;
     private List<Task> tasks;
 
-    public User() {
-    }
 
-    public User(int userId, String firstName, String lastName, String file, byte[] img, String emailId, String password, String role, List<Task> tasks) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.file = file;
-        this.img = img;
-        this.emailId = emailId;
-        this.password = password;
-        this.role = role;
-        this.tasks = tasks;
-    }
-
-    public byte[] getImg() {
-        return img;
-    }
-
-    public void setImg(byte[] img) {
-        this.img = img;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFile() {
-        return file;
-    }
-
-    public void setFile(String file) {
-        this.file = file;
-    }
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId=" + userId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", file='" + file + '\'' +
-                ", img=" + Arrays.toString(img) +
-                ", emailId='" + emailId + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
-                ", tasks=" + tasks +
-                '}';
-    }
 }
